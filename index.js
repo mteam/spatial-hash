73d622ca65336b2a852b8d38b5f2be912e93d7e8
Michal Miskernik <michal.miskernik@gmail.com>
2013-07-29 14:40:56 +0200

    v0.1.0

---
+++ index.js
@@ -0,0 +1,53 @@
+var Set = require('set');
+
+function SpatialHash(getRect, size) {
+  this.rect = getRect;
+  this.size = size || 50;
+  this.buckets = {};
+  this._bounds = {};
+}
+
+SpatialHash.prototype.insert = function(obj) {
+  var rect = this.rect(obj);
+  var bs = this.bounds(rect);
+
+  for (var x = bs.left; x <= bs.right; x++) {
+    for (var y = bs.top; y <= bs.bottom; y++) {
+      this.bucket(x, y).push(obj);
+    }
+  }
+};
+
+SpatialHash.prototype.collisions = function(obj) {
+  var rect = this.rect(obj);
+  var bs = this.bounds(rect);
+  var set = new Set();
+
+  for (var x = bs.left; x <= bs.right; x++) {
+    for (var y = bs.top; y <= bs.bottom; y++) {
+      var bucket = this.bucket(x, y);
+      bucket.forEach(set.add, set);
+    }
+  }
+
+  set.remove(obj);
+  return set.values();
+};
+
+SpatialHash.prototype.bucket = function(x, y) {
+  var hash = x + ';' + y;
+  return this.buckets[hash] || (this.buckets[hash] = []);
+};
+
+SpatialHash.prototype.bounds = function(rect) {
+  var bs = this._bounds;
+
+  bs.left = Math.floor(rect.left / this.size);
+  bs.top = Math.floor(rect.top / this.size);
+  bs.right = Math.ceil(rect.right / this.size) - 1;
+  bs.bottom = Math.ceil(rect.bottom / this.size) - 1;
+
+  return bs;
+};
+
+module.exports = SpatialHash;
---
+++ test.js
@@ -0,0 +1,106 @@
+var assert = require('assert');
+var SpatialHash = require('spatial-hash');
+
+describe('Spatial hash', function() {
+
+  function id(obj) { return obj; }
+  
+  describe('#bounds', function() {
+
+    var sh = new SpatialHash(id, 100);
+    
+    it('works for overlaping rects', function() {
+      var bs = sh.bounds({
+        left: 30, top: 20,
+        right: 140, bottom: 230
+      });
+
+      assert(bs.left == 0);
+      assert(bs.top == 0);
+      assert(bs.right == 1);
+      assert(bs.bottom == 2);
+    });
+
+    it('works for bordering rects', function() {
+      var bs = sh.bounds({
+        top: 0, left: 0,
+        bottom: 100, right: 100
+      });
+
+      assert(bs.left == 0);
+      assert(bs.top == 0);
+      assert(bs.right == 0);
+      assert(bs.bottom == 0);
+    });
+
+    it('works for negative rects', function() {
+      var bs = sh.bounds({
+        left: -50, top: -50,
+        right: 50, bottom: 50
+      });
+
+      assert(bs.left == -1);
+      assert(bs.top == -1);
+      assert(bs.right == 0);
+      assert(bs.bottom == 0);
+    });
+  
+  });
+
+  describe('#insert', function() {
+    
+    it('puts objects into buckets', function() {
+      var sh = new SpatialHash(id, 50);
+
+      sh.insert({
+        left: 25, top: 25,
+        right: 75, bottom: 75
+      });
+
+      sh.insert({
+        left: 75, top: 75,
+        right: 125, bottom: 125
+      });
+
+      assert(sh.buckets['0;0'].length == 1);
+      assert(sh.buckets['1;1'].length == 2);
+      assert(sh.buckets['2;2'].length == 1);
+    });
+  
+  });
+
+  describe('#collisions', function() {
+    
+    it('finds possible collisions', function() {
+      var sh = new SpatialHash(id, 50);
+      var object, possible;
+
+      sh.insert(object = {
+        left: 10, top: 10,
+        right: 60, bottom: 60
+      });
+
+      sh.insert(possible = {
+        left: 25, top: 75,
+        right: 75, bottom: 125
+      });
+
+      sh.insert({
+        left: 100, top: 20,
+        right: 200, bottom: 70
+      });
+
+      sh.insert({
+        left: -50, top: -50,
+        right: 0, bottom: 0
+      });
+
+      var collisions = sh.collisions(object);
+
+      assert(collisions.length == 1);
+      assert(collisions[0] == possible);
+    });
+  
+  });
+
+});
